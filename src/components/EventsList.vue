9bed8913914ffb12178f9e7376907199844515f4
barefootstache <webmaster@barefootstache.com>
2023-11-25 15:59:08 +0100

    feat: add filter out already passed events via date

--- src/components/EventsList.vue
+++ src/components/EventsList.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
-  import {format} from 'date-fns';
+  import {format, isAfter, subDays} from 'date-fns';
   import {ClubEvents, ClubEvent} from '../assets/events.db.ts';
   import {Clubs, Club} from '../assets/clubs.db.ts'
+
+  /**
+   * Filters events that are happening today and later.
+   */
+  const upcomingClubEvents = ClubEvents.filter((ev) => isAfter(ev.date, subDays(new Date(), 1)));
 
   /**
    * Gets the OSM Location URL.
@@ -37,7 +42,7 @@
   <div>
     <h2>Upcoming Events</h2>
     <ul>
-      <li v-for="ev in ClubEvents">
+      <li v-for="ev in upcomingClubEvents">
         <span style="font-weight: bold">{{ev.name}}</span><br>
         <span>{{format(ev.date, 'EEEE dd.MM.yyyy')}}</span><br>
         <span>{{ev.time}} - <a :href="getLocationUrl(ev)" target="_blank">{{ev.location}}</a></span><br>

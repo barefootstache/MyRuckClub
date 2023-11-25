431dca82136301eed3308bc2df344c6d54d621ca
barefootstache <webmaster@barefootstache.com>
2023-11-25 16:32:43 +0100

    feat: add new function to create events to reduce the amount of typing

--- src/assets/events.db.ts
+++ src/assets/events.db.ts
@@ -1,3 +1,5 @@
+import { Clubs } from "./clubs.db";
+
 export type ClubEvent = {
   clubId: string;
   date: Date;
@@ -6,6 +8,16 @@ export type ClubEvent = {
   long: number;
   name: string;
   time: string;
+}
+
+export const ClubEventInit: ClubEvent = {
+  clubId: '',
+  date: new Date(),
+  lat: 0,
+  location: 'TBA',
+  long: 0,
+  name: '',
+  time: '00:00'
 }
 
 export const ClubEvents: ClubEvent[] = [
@@ -18,33 +30,9 @@ export const ClubEvents: ClubEvent[] = [
     name: '12-Miler in Nürnberg',
     time: '11:00'
   },
-  {
-    clubId: 'ruckithannover',
-    date: new Date('2023-11-26'),
-    lat: 52.3894,
-    location: 'Freizeitheim Lister Turm, Hannover',
-    long: 9.7544,
-    name: 'Monthly Ruck Meetup',
-    time: '11:00'
-  },
-  {
-    clubId: 'auxruckers',
-    date: new Date('2023-11-28'),
-    lat: 48.36845,
-    location: 'Augsburg',
-    long: 10.89624,
-    name: 'Weekly Ruck Meetup',
-    time: '18:30'
-  },
-  {
-    clubId: 'munichruckingcrew',
-    date: new Date('2023-11-30'),
-    lat: 48.1779445,
-    location: 'BMW Welt Nord, München',
-    long: 11.5557917,
-    name: 'Weekly Ruck Meetup',
-    time: '19:00'
-  },
+  createClubEvent('2023-11-26', 'ruckithannover'),
+  createClubEvent('2023-11-28', 'auxruckers'),
+  createClubEvent('2023-11-30', 'munichruckingcrew'),
   {
     clubId: 'teutorucker',
     date: new Date('2023-12-03'),
@@ -54,13 +42,32 @@ export const ClubEvents: ClubEvent[] = [
     name: 'Monthly Ruck Meetup',
     time: '11:00'
   },
-  {
-    clubId: 'munichruckingcrew',
-    date: new Date('2023-12-16'),
-    lat: 48.1779445,
-    location: 'TBA',
-    long: 11.5557917,
-    name: 'Christmas Market Ruck',
-    time: '16:00'
-  },
+  createClubEvent('2023-12-05', 'auxruckers'),
+  createClubEvent('2023-12-07', 'munichruckingcrew', {name: 'MRC 12 Miler', time: '18:00'}),
+  createClubEvent('2023-12-12', 'auxruckers'),
+  createClubEvent('2023-12-14', 'munichruckingcrew'),
+  createClubEvent('2023-12-16', 'munichruckingcrew', {name: 'Christmas Market Ruck', time: '16:00', location: 'TBA'}),
+  createClubEvent('2023-12-19', 'auxruckers'),
+  createClubEvent('2023-12-21', 'munichruckingcrew'),
 ]
+
+/**
+ * Creates a club event.
+ * @param dateString - the date as a string
+ * @param clubId - the club id
+ * @param ev - custom club event properties
+ * @returns the new club event.
+ */
+export function createClubEvent(dateString: string, clubId: string, ev?: Partial<ClubEvent>): ClubEvent {
+  const club = Clubs.find(item => item.id === clubId);
+  const newEvent = {
+    clubId,
+    date: new Date(dateString),
+    lat: ev?.lat ? ev.lat : club?.default?.lat ? club.default.lat : ClubEventInit.lat,
+    location: ev?.location ? ev.location : club?.default?.location ? club.default.location : ClubEventInit.location,
+    long: ev?.long ? ev.long : club?.default?.long ? club.default.long : ClubEventInit.long,
+    name: ev?.name ? ev.name : club?.default?.name ? club.default.name : ClubEventInit.name,
+    time: ev?.time ? ev.time : club?.default?.time ? club.default.time : ClubEventInit.time,
+  }
+  return newEvent
+}

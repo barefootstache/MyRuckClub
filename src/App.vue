6d7133a9b34b378eaad4f956d9660df442d11fb6
barefootstache <webmaster@barefootstache.com>
2023-11-25 15:43:31 +0100

    fix: restructure front matter to include RSS feeds

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,19 @@ import EventsList from './components/EventsList.vue'
 
 <template>
   <h1>My Ruck Club</h1>
-    <p><em>My Ruck Club</em> is the first stop to get more info of your local ruck clubs. We are putting focus first in the DACH region.</p>
-    <p>This website is currently under construction!</p>
-    <p>We suggest to follow <a href="https://muenchen.social/@rucking" target="_blank">https://muenchen.social/@rucking</a> to get the latest development news. Or join the <a href="https://feddit.de/c/rucking" target="_blank">Rucking Community</a> on Lemmy.</p>
+  <p><em>My Ruck Club</em> is the first stop to get more info of your local ruck clubs.</p>
+  <p>We are putting focus first in the DACH region.</p>
+    <p>To get the development latest news we suggest:</p>
+    <ul>
+      <li>to follow <a href="https://muenchen.social/@rucking" target="_blank">https://muenchen.social/@rucking</a>,</li>
+      <li>or join the <a href="https://feddit.de/c/rucking" target="_blank">Rucking Community</a> on Lemmy.</li>
+      <li>or subscribe to their RSS feeds:
+        <ul>
+          <li><code>https://muenchen.social/users/rucking.atom</code></li>
+          <li><code>https://feddit.de/users/rucking.atom</code></li>
+        </ul>
+      </li>
+    </ul>
     <p>Or checkout one of these ruck clubs:</p>
     <Clubs/>
     <EventsList/>

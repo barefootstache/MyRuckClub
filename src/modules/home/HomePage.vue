be0380ca4aed04272c15e461e0f2294cddd2cdd3
barefootstache <webmaster@barefootstache.com>
2023-12-08 07:26:08 +0100

    feat: remove all but map on home; feat: update the navigation link style

--- src/modules/home/HomePage.vue
+++ src/modules/home/HomePage.vue
@@ -17,39 +17,27 @@ const hqIcon = L.icon({
 </script>
 
 <template>
-  <h1>My Ruck Club</h1>
-  <p><em>My Ruck Club</em> is the first stop to get more info of your local ruck clubs.</p>
+  <div class="mobile-container">
+    <div class="mobile-hide">
+      <h1>My Ruck Club</h1>
+      <p><em>My Ruck Club</em> is the first stop to get more info of your local ruck clubs.</p>
+    </div>
 
-  <div class="map-view">
-    <l-map ref="map" v-model:zoom="zoom" :center="[50.785, 9.547]">
-      <l-tile-layer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        layer-type="base"
-        name="OpenStreetMap"
-        attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
-      ></l-tile-layer>
+    <div class="map-view">
+      <l-map ref="map" v-model:zoom="zoom" :center="[50.785, 9.547]">
+        <l-tile-layer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          layer-type="base"
+          name="OpenStreetMap"
+          attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
+        ></l-tile-layer>
 
-      <div v-for="club in ClubsList">
-        <l-marker v-if="!club?.hide" :lat-lng="club.coordinates" :icon="hqIcon"> </l-marker>
-      </div>
-    </l-map>
+        <div v-for="club in ClubsList">
+          <l-marker v-if="!club?.hide" :lat-lng="club.coordinates" :icon="hqIcon"> </l-marker>
+        </div>
+      </l-map>
+    </div>
   </div>
-
-  <p>To get the development latest news we suggest:</p>
-  <ul>
-    <li>to follow <a href="https://muenchen.social/@rucking" target="_blank">https://muenchen.social/@rucking</a>,</li>
-    <li>or join the <a href="https://feddit.de/c/rucking" target="_blank">Rucking Community</a> on Lemmy.</li>
-    <li>or subscribe to their RSS feeds:
-      <ul>
-        <li><code>https://muenchen.social/users/rucking.atom</code></li>
-        <li><code>https://feddit.de/users/rucking.atom</code></li>
-      </ul>
-    </li>
-  </ul>
-  <p>Or checkout one of these ruck clubs:</p>
-  <Clubs/>
-  <h2>Upcoming Events</h2>
-  <EventsList/>
 </template>
 
 <style scoped>
@@ -60,8 +48,15 @@ const hqIcon = L.icon({
 }
 @media screen and (max-width: 800px) {
   .map-view {
-    height: 380px;
-    width: calc(100% - 20px);
+    height: 700px;
+    width: 400px;
+  }
+  .mobile-hide {
+    display: none;
+  }
+  .mobile-container {
+    height: calc(100% - 32px);
+    width: 100%;
   }
 }
 </style>

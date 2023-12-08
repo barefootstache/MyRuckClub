679c3b008fcd42ab2c80936a70075b036a36324e
barefootstache <webmaster@barefootstache.com>
2023-12-08 11:57:32 +0100

    refactor: create a service to hold all the functionality

--- src/modules/clubs/ClubPage.vue
+++ src/modules/clubs/ClubPage.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
   import { useRoute } from 'vue-router';
   import { format, isAfter, subDays } from 'date-fns';
-  import { unique } from 'radash'
   import "leaflet/dist/leaflet.css";
   import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
-  import { Clubs } from '../../db/clubs.db';
-  import { Club, Coordinates } from '../../business-logic/clubs.model';
+  import { ClubsDB } from '../../db/index.db';
+  import { Club } from '../../business-logic/clubs.model';
   import { EventsDB } from '../../db/index.db';
-  import { ClubEvent } from '../../business-logic/events.model';
   import { getPin } from '../../business-logic/osm.utils';
+  import { LocationService } from '../../services/location.service';
+  import { ClubEvent } from '../../business-logic/events.model';
 
   /**
    * Reference for `this.$route`.
@@ -18,110 +18,24 @@
   /**
    * Finds the referenced club by clubId.
    */
-  const club = Clubs.find((item) => item.id === route.params.id) as Club;
+  const club = ClubsDB.find((item) => item.id === route.params.id) as Club;
 
   /**
    * Filters events that are happening today and later.
    */
   const upcomingClubEvents = EventsDB.filter((item) => item.clubId === club.id).filter((item) => isAfter(item.date, subDays(new Date(), 1)));
-  const uniqueEventsLocations = getUniqueEventsLocations(upcomingClubEvents);
-  const allCoordinates = uniqueEventsLocations.map(ev => getCoordinates(ev)).concat([club.coordinates]);
-  const zoom = calcZoom(allCoordinates);
-
-  /**
-   * Gets the OSM Location URL.
-   * @param ev - the club event
-   * @returns the URL.
-   */
-  function getLocationClubUrl(ev: Club): string {
-    if(!ev.default){
-      return '#';
-    } else {
-      return `https://www.openstreetmap.org/?mlat=${ev.default.coordinates[0]}&mlon=${ev.default.coordinates[1]}#map=18/${ev.default.coordinates[0]}/${ev.default.coordinates[1]}`
-    }
-  }
-
-  /**
-   * Gets the OSM Location URL.
-   * @param ev - the club event
-   * @returns the URL.
-   */
-  function getLocationUrl(ev: ClubEvent): string {
-    return `https://www.openstreetmap.org/?mlat=${ev.coordinates[0]}&mlon=${ev.coordinates[1]}#map=18/${ev.coordinates[0]}/${ev.coordinates[1]}`
-  }
-
-  /** 
-   * Gets the coordinates of an club event.
-   * @param ev - the club event.
-   * @returns the coordinates.
-   */
-  function getCoordinates(ev: ClubEvent): Coordinates {
-    return ev.coordinates;
-  }
-
-  /** 
-   * Gets the unique events based off their lat/long data.
-   * @param evs - the club events
-   * @returns the unique club events.
-   */
-  function getUniqueEventsLocations(evs: ClubEvent[]): ClubEvent[] {
-    if(evs.length <= 1){
-      return evs;
-    }
-    return unique(evs, e => `${e.coordinates}`);
-  }
-
-  /** 
-   * Calculates the zoom relative to all provided coordinates.
-   * @param coordinates - the coordinates
-   * @returns the zoom.
-   */
-  function calcZoom(coordinates: Coordinates[]): number {
-    const initialZoom = 16;
-    const adjustSmallWindow = document.documentElement.clientWidth < 800 ? 1 : 0;
-    // Extract latitude and longitude arrays
-    const latitudes = coordinates.map(coord => coord[0]);
-    const longitudes = coordinates.map(coord => coord[1]);
-
-    // Calculate the delta coordinates
-    const deltaLatitude = Math.abs(Math.max(...latitudes)) - Math.abs(Math.min(...latitudes));
-    const deltaLongitude = Math.abs(Math.max(...longitudes)) - Math.abs(Math.min(...longitudes));
-    
-    const x = deltaLongitude * 1000;
-    const y = deltaLatitude * 1000;
-    const distance = Math.sqrt(x * x + y * y);
-
-    // Return the zoom
-    return initialZoom - (coordinates.length - 1) - Math.floor(distance / 50) - adjustSmallWindow;
-  }
-
-  /** 
-   * Calculates the center of the map.
-   * @param coordinates - the coordinates to center over
-   * @returns the centered coordinates.
-   */
-  function calcCenterMap(coordinates: Coordinates[]): Coordinates {
-    // Extract latitude and longitude arrays
-    const latitudes = coordinates.map(coord => coord[0]);
-    const longitudes = coordinates.map(coord => coord[1]);
-
-    // Calculate the center coordinates
-    const centerLatitude = (Math.max(...latitudes) + Math.min(...latitudes)) / 2;
-    const centerLongitude = (Math.max(...longitudes) + Math.min(...longitudes)) / 2;
-    
-    // Return the center coordinates
-    return [centerLatitude, centerLongitude];
-  }
-
+  const uniqueEventsLocations = LocationService.getUniqueEventsLocations(upcomingClubEvents);
+  const allCoordinates = uniqueEventsLocations.map((ev:ClubEvent) => LocationService.getCoordinates(ev)).concat([club.coordinates]);
+  const zoom = LocationService.calcZoom(allCoordinates);
 </script>
 
 <template>
   <div>
     <h2>{{club.name}}</h2>
-    <p>We typically meet at <a :href="getLocationClubUrl(club)" target="_blank">{{club?.default?.location || 'TBA'}}</a>.</p>
+    <p>We typically meet at <a :href="LocationService.getLocationClubUrl(club)" target="_blank">{{club?.default?.location || 'TBA'}}</a>.</p>
     
     <div class="map-view">
-      <l-map ref="map" v-model:zoom="zoom" :center="calcCenterMap(allCoordinates)">
+      <l-map ref="map" v-model:zoom="zoom" :center="LocationService.calcCenterMap(allCoordinates)">
         <l-tile-layer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           layer-type="base"
@@ -143,7 +57,7 @@
       <li v-for="ev in upcomingClubEvents">
         <span style="font-weight: bold">{{ev.name}}</span><br>
         <span>{{format(ev.date, 'EEEE dd.MM.yyyy')}}</span><br>
-        <span>{{ev.time}} - <a :href="getLocationUrl(ev)" target="_blank">{{ev.location}}</a></span><br>
+        <span>{{ev.time}} - <a :href="LocationService.getLocationUrl(ev)" target="_blank">{{ev.location}}</a></span><br>
         <span v-if="ev.clubId">Registration at <a :href="club.url" target="_blank">{{club.name}}</a></span>
         <span></span>
       </li>
@@ -171,5 +85,6 @@ li {
 .events-view {
   margin: auto;
   padding: 0;
+  list-style: none;
 }
 </style>

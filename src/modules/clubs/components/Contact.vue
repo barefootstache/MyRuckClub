66d287fd42abe712e649300b62775b355d352189
barefootstache <webmaster@barefootstache.com>
2023-12-19 18:37:46 +0100

    fix: styling on mobile

--- src/modules/clubs/components/Contact.vue
+++ src/modules/clubs/components/Contact.vue
@@ -22,20 +22,22 @@
 <template>
   <div class="container">
     <div class="primary-contact">
-      <p>
-        For more info contact us on our 
-        <v-chip variant="elevated" color="primary">
-          <a :href="preferred.url" target="_blank">
-            <v-icon start :icon="preferred.icon" color="white"></v-icon>
-            {{ preferred.name }}
-          </a>
-        </v-chip>
-      </p>
+      <span>For more info contact us on our</span>
+      <v-chip variant="elevated" color="primary">
+        <a :href="preferred.url" target="_blank">
+          <v-icon start :icon="preferred.icon" color="white"></v-icon>
+          <span>{{ preferred.name }}</span>
+        </a>
+      </v-chip>
     </div>
 
     <div class="more-contact" v-if="contacts.length > 0">
-      Or you can also find us on <v-chip variant="outlined" color="black" v-for="[social, url] in contacts">
-        <a v-if="social" :href="url" target="_blank" style="color: black"><v-icon start :icon="getIcon(social as keyof Contact)" color="red"></v-icon>{{ UtilsService.capitalize(social) }}</a>
+      <span>Or you can also find us on</span>
+      <v-chip variant="outlined" color="black" v-for="[social, url] in contacts">
+        <a v-if="social" :href="url" target="_blank" style="color: black">
+          <v-icon start :icon="getIcon(social as keyof Contact)" color="red"></v-icon>
+          <span>{{ UtilsService.capitalize(social) }}</span>
+        </a>
       </v-chip>
     </div>
   </div>
@@ -44,6 +46,9 @@
 <style scoped>
 .container {
   margin-top: 5px;
+}
+.v-chip {
+  margin-left: 5px;
 }
 .v-chip.text-black {
   background-color: rgba(255, 255, 255, 0.8);
@@ -56,5 +61,13 @@
 }
 .primary-contact .v-chip a {
   color: white;
+}
+@media screen and (max-width: 800px) {
+  .v-chip a span {
+    display: none;
+  }
+  .v-chip .v-icon--start {
+    margin-inline-end: -6px;
+  }
 }
 </style>

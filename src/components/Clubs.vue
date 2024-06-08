5cdd7b97f3afbafc38e83b7ded92aa774e1c850d
barefootstache <webmaster@barefootstache.com>
2024-06-08 22:14:12 +0200

    refactor(#45): autocomplete from #47

--- src/components/Clubs.vue
+++ src/components/Clubs.vue
@@ -1,25 +1,11 @@
 <script setup lang="ts">
   import { ClubsDB } from '@/db/index.db'
   import { alphabetical, unique } from 'radash';
-
-  const props = defineProps<{
-    search: string
-  }>();
-
-  const filterBySearch = props.search && props.search.length > 2 
-    ? ClubsDB.filter(c => c.country.includes(props.search) || c.name.includes(props.search)) 
-    : ClubsDB;
-  const alphabeticalSort = alphabetical(filterBySearch, (c) => {
-    console.log({filterBySearch});
-    return c.name
-  });
-  const countrySort = alphabetical(unique(filterBySearch.map(c => c.country)), c => c);
-
-  console.log({filterBySearch});
+  const alphabeticalSort = alphabetical(ClubsDB, c => c.name);
+  const countrySort = alphabetical(unique(ClubsDB.map(c => c.country)), c => c);
 </script>
 
 <template>
-  Search: {{props.search||''}}
   <div v-for="country in countrySort" id="country">
     <h3>{{country}}</h3>
     <ul>
--- src/modules/clubs/ClubsListPage.vue
+++ src/modules/clubs/ClubsListPage.vue
@@ -1,33 +1,8 @@
 <script setup lang="ts">
-  import { ref } from 'vue'
   import Clubs from '@/components/Clubs.vue'
-  import { ClubsDB } from '@/db/index.db'
-  import { unique } from 'radash';
-
-  const search = ref('');
-  const items = ClubsDB.map(c => c.name).concat(unique(ClubsDB.map(c=>c.country)));
-
-  function onAutocompleteChange(data:string):void {
-    console.log({data});
-    search.value = data ? data : search.value;
-  }
 </script>
 
 <template>
   <h2>Meet the Ruck Clubs</h2>
-  <v-row>
-    <!-- <v-col cols="9"> -->
-    <!--   <v-text-field label="Search" placeholder="Munich" variant="outlined" v-model="search"></v-text-field> -->
-    <!-- </v-col> -->
-    <!-- <v-col cols="3"> -->
-    <!--   <v-btn append-icon="mdi-magnify" color="primary" size="x-large">Search</v-btn> -->
-    <!-- </v-col> -->
-    <v-col cols="12">
-      <v-autocomplete :items="items" 
-        label="Search" :clearable="true"
-        @update:search="onAutocompleteChange($event)"
-        ></v-autocomplete>
-    </v-col>
-  </v-row>
-  <Clubs :search="search"></Clubs>
+  <Clubs></Clubs>
 </template>

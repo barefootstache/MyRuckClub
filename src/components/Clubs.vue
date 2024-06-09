fe0c16dee703d0acc1106c4a78c0de6a9c009a96
barefootstache <webmaster@barefootstache.com>
2024-06-09 17:45:30 +0200

    feat(#47): can filter clubs over a search term
    - feat: can filter clubs over a search term. It permits both over name
      and country.
    - refactor: the indent of the style section
    - docs: add zod.dev link for future schema validation

--- src/components/Clubs.vue
+++ src/components/Clubs.vue
@@ -1,15 +1,43 @@
 <script setup lang="ts">
-  import { ClubsDB } from '@/db/index.db'
-  import { alphabetical, unique } from 'radash';
-  const alphabeticalSort = alphabetical(ClubsDB, c => c.name);
-  const countrySort = alphabetical(unique(ClubsDB.map(c => c.country)), c => c);
+  import { Club } from '@/business-logic/clubs.model';
+  import { unique } from 'radash';
+  import { computed } from 'vue';
+
+  const props = withDefaults(defineProps<{
+    items: Club[],
+    search: string
+  }>(),{
+    search: ''
+  });
+
+  const filteredItems = computed<Club[]>(
+    () => {
+      if(!props.search) return props.items;
+      const searchLower = props.search.toLowerCase();
+      return props.items.filter(item =>
+        item.name.toLowerCase().includes(searchLower) ||
+        item.country.toLowerCase().includes(searchLower)
+      );
+  });
+
+  const sortByCountryThenName = computed<Club[]>(
+    () => {
+      return filteredItems.value.slice().sort((a,b) => {
+        const countryComparison = a.country.localeCompare(b.country);
+        if (countryComparison !== 0) {
+          return countryComparison;
+        }
+        return a.name.localeCompare(b.name);
+      })
+    }
+  )
 </script>
 
-<template>
-  <div v-for="country in countrySort" id="country">
+<template :items="sortByCountryThenName">
+  <div v-for="country in unique(sortByCountryThenName.map(c => c.country))" id="country">
     <h3>{{country}}</h3>
     <ul>
-      <li v-for="club in alphabeticalSort.filter(c => c.country === country)">
+      <li v-for="club in sortByCountryThenName.filter(c => c.country === country)">
         <router-link v-if="!club.hide" :to="{ name: 'Club', params: {id: club.id}}">{{ club.name }}</router-link>
       </li>
     </ul>
@@ -17,14 +45,15 @@
 </template>
 
 <style>
-ul {
-  list-style-type: none;
-  padding: 0;
-  margin: auto;
-}
-h3 {
-  border-bottom: 1px rgba(var(--v-theme-surface), 0.87) solid;
-  border-top: 1px rgba(var(--v-theme-surface), 0.1) solid;
-  margin-top: 16px;
-}
+  ul {
+    list-style-type: none;
+    padding: 0;
+    margin: auto;
+  }
+
+  h3 {
+    border-bottom: 1px rgba(var(--v-theme-surface), 0.87) solid;
+    border-top: 1px rgba(var(--v-theme-surface), 0.1) solid;
+    margin-top: 16px;
+  }
 </style>
--- src/modules/clubs/ClubsListPage.vue
+++ src/modules/clubs/ClubsListPage.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
   import Clubs from '@/components/Clubs.vue'
+  import { ClubsDB } from '@/db/index.db'
 </script>
 
 <template>
   <h2>Meet the Ruck Clubs</h2>
-  <Clubs></Clubs>
+  <Clubs :items="ClubsDB" search="ruck"></Clubs>
 </template>

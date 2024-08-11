487a6d9dcdec019860936d11bdd141709732a960
barefootstache <webmaster@barefootstache.com>
2024-08-11 19:17:52 +0200

    refactor(33): format

--- src/config/turso.ts
+++ src/config/turso.ts
@@ -1,6 +1,6 @@
-import {createClient} from '@libsql/client/web';
+import { createClient } from '@libsql/client/web';
 
 export const turso = createClient({
   url: import.meta.env.VITE_TURSO_DATABASE_URL,
-  authToken: import.meta.env.VITE_TURSO_AUTH_TOKEN
+  authToken: import.meta.env.VITE_TURSO_AUTH_TOKEN,
 });

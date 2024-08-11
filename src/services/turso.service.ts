44c3adfc0328d8316eb5a5f816a67461a6c788a4
barefootstache <webmaster@barefootstache.com>
2024-08-11 19:16:27 +0200

    feat(33): add services regarding clubs

--- src/services/turso.service.ts
+++ src/services/turso.service.ts
@@ -1,15 +1,58 @@
-import { ClubEvent, EventType } from "@/business-logic";
-import { turso } from "@/config/turso";
-import { Row } from "@libsql/client";
-import { format } from "date-fns";
+import {
+  AssociationType,
+  Club,
+  ClubEvent,
+  Contact,
+  Coordinates,
+  Country,
+  Default,
+  EventType,
+  SocialMediaContent,
+  Timezone,
+} from '@/business-logic';
+import { turso } from '@/config/turso';
+import { Row } from '@libsql/client';
+import { format } from 'date-fns';
 
 export class TursoService {
-  static async getFutureEvents(): Promise<ClubEvent[]> {
-    const result = (await turso.execute('SELECT * FROM events WHERE startsAt > CURRENT_TIMESTAMP')).rows.map(row => TursoService.createEventFromTurso(row));
+  static async getClubById(clubId: string): Promise<Club> {
+    const result = (
+      await turso.execute({
+        sql: 'SELECT * FROM clubs WHERE id = (:clubId)',
+        args: { clubId },
+      })
+    ).rows.map((row) => TursoService.parseAsClub(row))[0];
+    return result as Club;
+  }
+
+  static async getAllClubs(): Promise<Club[]> {
+    const result = (await turso.execute('SELECT * FROM clubs')).rows.map(
+      (row) => TursoService.parseAsClub(row)
+    );
     return result;
   }
 
-  private static createEventFromTurso(row: Row): ClubEvent {
+  static async getEventsByClubId(clubId: string): Promise<ClubEvent[]> {
+    const result = (
+      await turso.execute({
+        sql: 'SELECT * FROM events WHERE startsAt > CURRENT_TIMESTAMP AND clubId = (:clubId)',
+        args: { clubId },
+      })
+    ).rows.map((row) => TursoService.parseAsClubEvent(row));
+
+    return result;
+  }
+
+  static async getFutureEvents(): Promise<ClubEvent[]> {
+    const result = (
+      await turso.execute(
+        'SELECT * FROM events WHERE startsAt > CURRENT_TIMESTAMP'
+      )
+    ).rows.map((row) => TursoService.parseAsClubEvent(row));
+    return result;
+  }
+
+  private static parseAsClubEvent(row: Row): ClubEvent {
     const startsAt = new Date(row['startsAt'] as string);
     return {
       url: row['url'] as string,
@@ -22,7 +65,27 @@ export class TursoService {
       duration: JSON.parse(row['duration'] as string) ?? { hours: 3 },
       coordinates: JSON.parse(row['coordinates'] as string),
       date: startsAt,
-      time: format(startsAt, 'HH:mm')
-    }
+      time: format(startsAt, 'HH:mm'),
+    };
+  }
+
+  private static parseAsClub(row: Row): Club {
+    return {
+      coordinates: JSON.parse(row['coordinates'] as string) as Coordinates,
+      id: row['id'] as string,
+      name: row['name'] as string,
+      hide: Boolean(row['hide'] as number),
+      contact: JSON.parse(row['contact'] as string) as Contact,
+      country: row['country'] as Country,
+      default: JSON.parse(row['default'] as string) as Default,
+      hasLogo: Boolean(row['hasLogo'] as number),
+      timezone: JSON.parse(row['timezone'] as string) as Timezone,
+      associations: JSON.parse(
+        row['associations'] as string
+      ) as AssociationType[],
+      socialMediaContent: JSON.parse(
+        row['socialMediaContent'] as string
+      ) as SocialMediaContent,
+    };
   }
 }

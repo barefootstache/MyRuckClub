b9bdb1eb39758c5f75e46372a58b68ca4bac72f4
barefootstache <webmaster@barefootstache.com>
2024-08-11 21:06:17 +0200

    feat(33): add functions for type testing

--- src/business-logic/clubs.utils.ts
+++ src/business-logic/clubs.utils.ts
@@ -1,4 +1,4 @@
-import { Contact } from '@/business-logic';
+import { Club, Contact } from '@/business-logic';
 
 /**
  * Gets the preferred contact URL.
@@ -7,4 +7,17 @@ import { Contact } from '@/business-logic';
  */
 export function getContactUrl(contact: Contact): string {
   return contact[contact.preferred] as string;
+}
+
+/**
+ * Checks if the `obj` is of type `Club`.
+ * @param obj - the object to test
+ * @returns the truth of the type
+ */
+export function isClub(obj:any): obj is Club {
+  if(typeof obj !== 'object' || obj === null) return false;
+  if(!Array.isArray(obj.coordinates) || obj.coordinates.length !== 2 || typeof obj.coordinates[0] !== 'number' || typeof obj.coordinates[1] !== 'number') return false;
+  if(typeof obj.id !== 'string') return false;
+  if(typeof obj.name !== 'string') return false;
+  return true;
 }

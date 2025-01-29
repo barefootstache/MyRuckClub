f1b9d52bc3a0847cefabe901a3303aa9c01c4d53
barefootstache <webmaster@barefootstache.com>
2025-01-29 06:29:23 +0100

    feat: update script

--- projects/db/generate-sql-transactions.js
+++ projects/db/generate-sql-transactions.js
@@ -1,10 +1,10 @@
 const { format, addDays } = require("date-fns");
 
-const refernceId = 163;
+const refernceId = 348;
 // month 0 is January
-const startDate = new Date(2024, 9, 25);
-const startTime = '17:30';
-const duplicateCount = 8;
+const startDate = new Date(2025, 1, 12);
+const startTime = '18:00';
+const duplicateCount = 10;
 
 function duplicateRow(date, time) {
   return `INSERT INTO "events" ("clubid", "coordinates", "startsAt", "duration", "inSummer", "location", "name", "type", "url") SELECT "clubid", "coordinates", '${format(date, 'yyyy-MM-dd')} ${time}', "duration", "inSummer", "location", "name", "type", "url" FROM "events" WHERE "id" = ${refernceId};\n`;

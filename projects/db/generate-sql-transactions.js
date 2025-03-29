e89a8069f3c38225ead5f3c3a8b59c35a4a3fed3
barefootstache <webmaster@barefootstache.com>
2025-03-29 09:49:41 -0700

    feat: calculate all next events for clubs in one go

--- projects/db/generate-sql-transactions.js
+++ projects/db/generate-sql-transactions.js
@@ -1,18 +1,76 @@
 const { format, addDays } = require("date-fns");
 
-const refernceId = 348;
 // month 0 is January
-const startDate = new Date(2025, 1, 12);
-const startTime = '18:00';
-const duplicateCount = 10;
+const frequency = [
+  {
+    clubId: 'auxruckers',
+    time: '18:30',
+    date: new Date(2025, 2, 25),
+    referenceId: '347'
+  },
+  {
+    clubId: 'goruck_club_franken',
+    time: '19:00',
+    date: new Date(2025, 2, 24),
+    referenceId: '316'
+  },
+  {
+    clubId: 'goruck_club_ythsie',
+    time: '18:00',
+    date: new Date(2025, 2, 26),
+    referenceId: '354'
+  },
+  {
+    clubId: 'herzschlagruckerz',
+    time: '11:00',
+    date: new Date(2025, 2, 29),
+    referenceId: '341'
+  },
+  {
+    clubId: 'munichruckingcrew',
+    time: '19:00',
+    date: new Date(2025, 2, 27),
+    referenceId: '331'
+  },
+  {
+    clubId: 'polarisruckingcrew',
+    time: '12:00',
+    date: new Date(2025, 2, 29),
+    referenceId: '311'
+  },
+  {
+    clubId: 'ruhr_ruckers',
+    time: '18:00',
+    date: new Date(2025, 2, 27),
+    referenceId: '326'
+  },
+  {
+    clubId: 'teutorucker',
+    time: '17:30',
+    date: new Date(2025, 2, 27),
+    referenceId: '321'
+  },
+  {
+    clubId: 'ybbsvalleyruckers',
+    time: '17:30',
+    date: new Date(2025, 2, 28),
+    referenceId: '336'
+  },
+]
 
-function duplicateRow(date, time) {
-  return `INSERT INTO "events" ("clubid", "coordinates", "startsAt", "duration", "inSummer", "location", "name", "type", "url") SELECT "clubid", "coordinates", '${format(date, 'yyyy-MM-dd')} ${time}', "duration", "inSummer", "location", "name", "type", "url" FROM "events" WHERE "id" = ${refernceId};\n`;
+// Makes 10 duplicates since starts with 1 and ends with < 11
+const duplicateCount = 11;
+
+function duplicateRow(referenceId, date, time) {
+  return `INSERT INTO "events" ("clubid", "coordinates", "startsAt", "duration", "inSummer", "location", "name", "type", "url") SELECT "clubid", "coordinates", '${format(date, 'yyyy-MM-dd')} ${time}', "duration", "inSummer", "location", "name", "type", "url" FROM "events" WHERE "id" = ${referenceId};\n`;
 }
 
-let response = '';
-for (let ii = 1; ii < duplicateCount; ii++) {
-  response += duplicateRow(addDays(startDate, 7 * ii), startTime);
-}
+let response = frequency.map(item => {
+  let responseItem = `-- ${item.clubId}\n`;
+  for (let ii = 1; ii < duplicateCount; ii++) {
+    responseItem += duplicateRow(item.referenceId, addDays(item.date, 7 * ii), item.time);
+  }
+  return responseItem
+}).reduce((prev, curr) => prev + curr);
 
 console.log(response)
